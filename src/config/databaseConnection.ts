882a7c2ee8983474853f56ad6c78409db5562be9
sidghimire <siddharthaghimire@gmail.com>
2023-12-01 02:13:52 +0545

    Update

--- src/config/databaseConnection.ts
+++ src/config/databaseConnection.ts
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose,{ConnectOptions} from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config()
 export default function connectDatabase() {
     const Password=process.env.PASSWORD
     console.log(Password)
     mongoose.connect(
-        `mongodb://siddharthaghimire:${Password}@ac-5fhopgk-shard-00-00.eydnij8.mongodb.net:27017,ac-5fhopgk-shard-00-01.eydnij8.mongodb.net:27017,ac-5fhopgk-shard-00-02.eydnij8.mongodb.net:27017/?ssl=true&replicaSet=atlas-mlqlwr-shard-0&authSource=admin&retryWrites=true&w=majority`)
+        `mongodb://siddharthaghimire:${Password}@ac-5fhopgk-shard-00-00.eydnij8.mongodb.net:27017,ac-5fhopgk-shard-00-01.eydnij8.mongodb.net:27017,ac-5fhopgk-shard-00-02.eydnij8.mongodb.net:27017/?ssl=true&replicaSet=atlas-mlqlwr-shard-0&authSource=admin&retryWrites=true&w=majority` , { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions)
         .then(() => {
             console.log('Connected to MongoDB');
         })
