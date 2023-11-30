5ecf9b9d3927f5e4fc0f5f24308ea92e4fac3217
sidghimire <siddharthaghimire@gmail.com>
2023-12-01 00:32:46 +0545

    Update

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config()
-import feedbackRouter from './src/routes/feedback.js'
-import travelRecord from './src/routes/travelRecord.js'
-import reportRecord from './src/routes/report.js'
-import authRouter from './src/routes/auth.js'
+
 import connectDatabase from './src/config/databaseConnection.js'
 const app = express();
 const port = process.env.PORT;
@@ -18,8 +15,3 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.json({ message: 'Sid' })
 })
-
-app.use('/auth', authRouter)
-app.use('/feedback', feedbackRouter)
-app.use('/travelRecord', travelRecord)
-app.use('/report', reportRecord)
